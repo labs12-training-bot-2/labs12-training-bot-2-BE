b6bb78d37e81c5264c6ddd92ef2822b170008955
Kingaa6 <Kingaa6@gmail.com>
2019-03-21 13:41:45 -0700

    Adjust notification interval to 11:30am M-F

--- twilio/startSystem.js
+++ twilio/startSystem.js
@@ -3,22 +3,24 @@ const moment = require('moment');
 
 const runNotification = require('./runNotification');
 
-// node-cron start function for notification system, notification interval set to one minute.
+// node-cron start function for notification system
 const schedulerFactory = function() {
   return {
     start: function() {
+      console.log('Before job instantiation');
       new CronJob(
-        '00 * * * * *',
+        '00 30 11 * * 1-5', // interval set to 11:30am M-F.
+        // '00 * * * * *'  // 1 minute interval for testing notification system.
         function() {
-          console.log(
-            'Running Send Notifications Worker for ' + moment().format()
-          );
-          runNotification.run();
+          const d = new Date();
+          console.log('onTick:', d);
         },
         null,
         true,
         ''
       );
+      console.log('After job instantiation');
+      runNotification.run();
     }
   };
 };
