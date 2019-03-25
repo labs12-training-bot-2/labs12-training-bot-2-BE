d4760a7114631a8bbbdfe5fcc639b64a1960dd7e
Kingaa6 <Kingaa6@gmail.com>
2019-03-25 14:46:05 -0700

    Add get post by id endpoint

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
   }
 })
 
-//PUT team member information
+// PUT post information
 router.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -34,5 +34,16 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+// GET post by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Posts.findById(id);
+    res.status(200).json({ post });
+  } catch (err) {
+    res.status(500).json({ message: "A network error occurred" });
+  }
+})
 
 module.exports = router;
