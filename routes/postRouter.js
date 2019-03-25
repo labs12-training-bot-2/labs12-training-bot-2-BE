cced712ed9c161d5cb33db5a48630cde3bfa1666
Kingaa6 <Kingaa6@gmail.com>
2019-03-25 14:28:22 -0700

    Add put request for post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -23,5 +23,16 @@ router.post("/", async (req, res) => {
   }
 })
 
+//PUT team member information
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedPost = await Posts.update(id, req.body);
+    res.status(200).json({ updatedPost })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 
 module.exports = router;
