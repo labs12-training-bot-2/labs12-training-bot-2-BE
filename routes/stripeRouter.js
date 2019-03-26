2e1f354232319f8d1fa5a1775d192e74972ae273
Mike Landers <landers.mike@gmail.com>
2019-03-26 10:39:29 -0700

    create user

--- routes/stripeRouter.js
+++ routes/stripeRouter.js
@@ -2,12 +2,29 @@
 const router = require('express').Router();
 const stripe = require('stripe')('sk_test_I3A5cCkzbD6C7HqqHSt7uRHH00ht9noOJw');
 
+// const Users = require('../database/Helpers/user-model.js');
+
 // stripe.charges.retrieve('ch_1EHzLXChlDwQi04Iono5543P', {
 // 	api_key: 'sk_test_I3A5cCkzbD6C7HqqHSt7uRHH00ht9noOJw',
 // });
 
 router.post('/', async (req, res) => {
+	// const { id } = req.body;
+
 	try {
+		// const user = await Users.findById(id);
+		// if (!user.id)
+		let { customer } = await stripe.customers.create(
+			{
+				description: `Customer for ${email}`,
+				source: 'tok_amex', // obtained with Stripe.js
+				email: { email },
+			},
+			function(err, customer) {
+				// asynchronously called
+				console.log(customer);
+			}
+		);
 		let { status } = await stripe.charges.create({
 			amount: 2000,
 			currency: 'usd',
@@ -15,27 +32,10 @@ router.post('/', async (req, res) => {
 			source: req.body,
 		});
 
-		res.json({ status });
+		res.json({ status, customer });
 	} catch (err) {
 		res.status(500).end();
 	}
 });
 
-router.post('/', async (req, res) => {
-	try {
-		let response = await axios.post('api.stripe.com/v1/subscriptions', {
-			amount: 2000,
-			currency: 'usd',
-			description: 'An example charge',
-			source: req.body,
-		});
-
-		res.json({ status });
-	} catch (err) {
-		res.status(500).end();
-	}
-});
-
-//api.stripe.com
-
-https: module.exports = router;
+module.exports = router;
