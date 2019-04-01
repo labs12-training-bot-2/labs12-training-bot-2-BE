d5e987493c9d8b9a2dc02df86d27366792725be8
Mike Landers <landers.mike@gmail.com>
2019-04-01 10:20:51 -0700

    WIP

--- routes/stripeRouter.js
+++ routes/stripeRouter.js
@@ -4,141 +4,95 @@ const stripe = require('stripe')('sk_test_I3A5cCkzbD6C7HqqHSt7uRHH00ht9noOJw');
 
 const Users = require('../database/Helpers/user-model.js');
 
-async function subscribe(stripeID, userID, plan) {
+// pass in stripeID
+async function getStripeUser(stripeID) {
 	try {
-		// let subID;
-		let subID = await stripe.customers.retrieve(stripeID);
-		console.log('subID', subID);
-
-		if (subID.subscriptions.total_count === 0) {
-			let sub = await stripe.subscriptions.create(
-				{ customer: stripeID, items: [{ plan: plan }] },
-				function(err, subscription) {
-					console.log('subscription', subscription);
-					return subscription;
-				}
-			);
-			console.log('sub', sub);
-		} else {
-			subID = subID.subscriptions.data[0].id;
-			let cancelled = await stripe.subscriptions.del(subID);
-			let sub = await stripe.subscriptions.create({
-				customer: stripeID,
-				items: [
-					{
-						plan: plan,
-					},
-				],
-			});
-			if (sub) {
-				let id;
-				if (plan === 'plan_EmJaXZor4Ef3co') {
-					id = 3;
-				} else if (plan === 'plan_EmJallrSdkqpPS') {
-					id = 2;
-				}
-				let changes = { accountTypeID: id };
-				Users.updateUser(userID, changes);
-				return id;
-			}
-		}
+		return await stripe.customers.retrieve(stripeID);
 	} catch (error) {
-		console.log('error', error);
+		console.log(error);
 	}
 }
-const unsubscribe = async (stripeID, userID) => {
-	try {
-		return await stripe.customers.retrieve(stripeID, async function(err, customer) {
-			// asynchronously called
-			let subID = customer.subscriptions.data[0].id; //Gets the one subscription ID the customer can have
 
-			// API for removing subscription
-			let confirmation = await stripe.subscriptions.del(subID);
-			// need to add back in the update to accountID in database. Would prefer it to be handled by the actions though
-			console.log('confirmation', confirmation);
-			return 1;
-		});
+// pass in stripeID and plan ID
+async function subscribe(stripeID, plan) {
+	try {
+		return await stripe.subscriptions.create({ customer: stripeID, items: [{ plan: plan }] });
 	} catch (error) {
 		console.log(error);
 	}
-};
-async function registerSubscribe(name, email, token, userID, plan) {
-	// not currently updating the store with the new account typeid
+}
+
+// pass in subscription ID
+async function unsubscribe(userID, stripe) {
 	try {
-		let customer = await stripe.customers.create({
-			description: name,
-			email: email,
-			source: token, // obtained with Stripe.js
-		});
+		let customer = await getStripeUser(stripe);
+		let subID = customer.subscriptions.data[0].id;
+		await stripe.subscriptions.del(subID);
+		updateUserAccountType(userID, plan);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-		const changes = { stripe: customer.id };
-		Users.updateUser(userID, changes);
-
-		let sub = await stripe.subscriptions.create({
-			customer: customer.id,
-			items: [
-				{
-					plan: plan,
-				},
-			],
-		});
-		if (sub) {
-			let id;
-			if (plan === 'plan_EmJaXZor4Ef3co') {
-				id = 3;
-			} else if (plan === 'plan_EmJallrSdkqpPS') {
-				id = 2;
+async function register(email) {
+	try {
+		await stripe.customers.create(
+			{
+				description: email,
+				source: token, // obtained with Stripe.js
+			},
+			function(err, customer) {
+				// asynchronously called
 			}
-			let changes = { accountTypeID: id };
-			Users.updateUser(userID, changes);
-			return id;
-		}
-
-		return { stripe: customer.id, subscription: sub };
+		);
 	} catch (error) {
 		console.log(error);
 	}
+}
 
-	///////====================
-	// API for creating a customer in Stripe's system
-	// stripe.customers.create(
-	// 	{
-	// 		description: name,
-	// 		email: email,
-	// 		source: token, // obtained with Stripe.js
-	// 	},
-	// 	function(err, customer) {
-	// 		// asynchronously called
-	// 		// add strip user_id to the database
-	// 		const changes = { stripe: customer.id };
-	// 		Users.updateUser(userID, changes);
+function updateUserAccountType(userID, plan) {
+	console.log('update user account', plan);
+	let accountTypeID;
+	if (plan === 'plan_EmJallrSdkqpPS') {
+		accountTypeID = 2;
+	} else if (plan === 'plan_EmJaXZor4Ef3co') {
+		accountTypeID = 3;
+	} else {
+		accountTypeID = 1;
+	}
+	console.log('accountTypeID', accountTypeID);
 
-	// 		// calls subscribe function to subsribe the user
-	// 		let sub = subscribe(customer.id, userID);
-	// 		return sub;
-	// 	}
-	// );
+	Users.updateUser(userID, { accountTypeID: accountTypeID });
 }
 
 router.post('/', async (req, res) => {
 	const { token, name, email, userID, stripe, plan } = req.body;
 	if (stripe) {
 		try {
-			console.log('Subscribe Only');
+			let customer = await getStripeUser(stripe);
 
-			// calls subscribe function to subsribe the user
-			let sub = await subscribe(stripe, userID, plan);
-			res.status(200).json(sub);
+			if (customer.subscriptions.total_count === 0) {
+				let subscription = await subscribe(stripe, plan);
+				updateUserAccountType(userID, plan);
+				res.status(200).json(subscription);
+			} else {
+				let subID = customer.subscriptions.data[0].id;
+				console.log('subID', subID);
+				await unsubscribe(userID, subID);
+				updateUserAccountType(userID, plan);
+				let subscription = await subscribe(stripe, plan);
+				updateUserAccountType(userID, plan);
+
+				res.status(200).json(subscription);
+			}
 		} catch (err) {
+			console.log(err);
 			res.status(500).end();
 		}
 	} else {
 		try {
-			console.log('Register & Subscribe');
+			let sub = register(email, token);
 
-			// calls registerSubscribe function to register and then subsribe the user to the plan
-			let sub = registerSubscribe(name, email, token, userID, plan);
-			console.log('sub', sub);
 			res.status(200).json(sub);
 		} catch (err) {
 			res.status(500).end();
@@ -148,7 +102,7 @@ router.post('/', async (req, res) => {
 
 router.post('/unsubscribe', async (req, res) => {
 	const { userID, stripe } = req.body;
-	console.log('req body', req.body);
+	// console.log('req body', req.body);
 	if (stripe) {
 		try {
 			let res = unsubscribe(stripe, userID);
@@ -162,44 +116,60 @@ router.post('/unsubscribe', async (req, res) => {
 	}
 });
 
-router.get('/plans', (req, res) => {
-	stripe.plans.list(
-		{
-			limit: 3,
-			product: 'prod_EmJZbRNGEjlOY4',
-		},
-		function(err, plans) {
-			// console.log('plans', plans.data);
-			res.send(plans.data);
-		}
-	);
+router.get('/plans', async (req, res) => {
+	try {
+		stripe.plans.list(
+			{
+				limit: 3,
+				product: 'prod_EmJZbRNGEjlOY4',
+			},
+			function(err, plans) {
+				// console.log('plans', plans.data);
+				res.send(plans.data);
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
 });
-router.get('/subscriptions', (req, res) => {
-	stripe.subscriptions.list(
-		{
-			limit: 3,
-		},
-		function(err, subscriptions) {
-			res.send(subscriptions.data);
-		}
-	);
+router.get('/subscriptions', async (req, res) => {
+	try {
+		await stripe.subscriptions.list(
+			{
+				limit: 3,
+			},
+			function(err, subscriptions) {
+				res.send(subscriptions.data);
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
 });
-router.get('/customer/plan', (req, res) => {
+router.get('/customer/plan', async (req, res) => {
 	console.log('customer plan', req.body);
-	stripe.customers.retrieve(req.body.stripe, function(err, customer) {
-		res.send(customer);
-	});
+	try {
+		await stripe.customers.retrieve(req.body.stripe, function(err, customer) {
+			res.send(customer);
+		});
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 // Cancel subscription route
 
 router.post('/paymentintent', async (req, res) => {
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: 1099,
-		currency: 'usd',
-		payment_method_types: ['card'],
-	});
-	res.json({ paymentIntent });
+	try {
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: 1099,
+			currency: 'usd',
+			payment_method_types: ['card'],
+		});
+		res.json({ paymentIntent });
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 module.exports = router;
