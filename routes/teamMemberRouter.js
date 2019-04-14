455d1977e4e4e4c660e8c3c086bb9f8d78c28118
Kingaa6 <Kingaa6@gmail.com>
2019-04-14 11:33:48 -0700

    DB fix user_ID to userID to match across all tables

--- routes/teamMemberRouter.js
+++ routes/teamMemberRouter.js
@@ -1,15 +1,15 @@
 //Dependencies
-const router = require("express").Router();
-const moment = require("moment");
+const router = require('express').Router();
+const moment = require('moment');
 
 //Models
-const TeamMember = require("../database/Helpers/teamMember-model");
-const TrainingSeries = require("../database/Helpers/trainingSeries-model");
+const TeamMember = require('../database/Helpers/teamMember-model');
+const TrainingSeries = require('../database/Helpers/trainingSeries-model');
 const Notifications = require('../database/Helpers/notifications-model');
 //Routes
 
 // GET all team members in system (not a production endpoint)
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const teamMembers = await TeamMember.find();
     res.status(200).json({ teamMembers });
@@ -19,7 +19,7 @@ router.get("/", async (req, res) => {
 });
 
 // GET a team member by teamMemberId
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
@@ -31,12 +31,12 @@ router.get("/:id", async (req, res) => {
 
     res.status(200).json({ teamMember, assignments });
   } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
+    res.status(500).json({ message: 'A network error occurred' });
   }
 });
 
 // POST a new team member
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const {
       firstName,
@@ -44,7 +44,7 @@ router.post("/", async (req, res) => {
       jobDescription,
       email,
       phoneNumber,
-      user_ID
+      userID
     } = req.body;
 
     if (
@@ -53,9 +53,9 @@ router.post("/", async (req, res) => {
       !jobDescription ||
       !email ||
       !phoneNumber ||
-      !user_ID
+      !userID
     ) {
-      res.status(400).json({ error: "Client must provide all fields." });
+      res.status(400).json({ error: 'Client must provide all fields.' });
     } else {
       const newTeamMember = await TeamMember.add(req.body);
       res.status(201).json({ newTeamMember });
@@ -66,7 +66,7 @@ router.post("/", async (req, res) => {
 });
 
 // PUT team member information
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const updatedTeamMember = await TeamMember.update(id, req.body);
@@ -78,14 +78,14 @@ router.put("/:id", async (req, res) => {
 });
 
 // DELETE a team member
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const deleted = await TeamMember.remove(id);
     if (deleted > 0) {
-      res.status(200).json({ message: "The resource has been deleted." });
+      res.status(200).json({ message: 'The resource has been deleted.' });
     } else {
-      res.status(404).json({ error: "The resource could not be found." });
+      res.status(404).json({ error: 'The resource could not be found.' });
     }
   } catch (err) {
     res.status(500).json(err);
@@ -93,27 +93,27 @@ router.delete("/:id", async (req, res) => {
 });
 
 // Assigns one or multiple team members to training series with the same start date
-router.post("/assign", async (req, res) => {
+router.post('/assign', async (req, res) => {
   try {
     // store array of objects in a new variable
     const { startDate, trainingSeriesID } = req.body;
     const incomingAssignments = req.body.assignments;
 
     if (!startDate || !trainingSeriesID) {
-      return res.status(400).json({error: "Start date and training series ID required."})
-    };
+      return res
+        .status(400)
+        .json({ error: 'Start date and training series ID required.' });
+    }
 
     // for each object in array, perform a series of tasks
     incomingAssignments.forEach(async assignment => {
       try {
-
         const newObject = {
           startDate: startDate,
           trainingSeries_ID: trainingSeriesID,
           teamMember_ID: assignment
+        };
 
-        }
-      
         // 1. assign member to training series, return information
         await TeamMember.addToTrainingSeries(newObject);
 
@@ -134,7 +134,7 @@ router.post("/assign", async (req, res) => {
             link: post.link,
             daysFromStart: post.daysFromStart,
             sendDate: moment(startDate)
-              .add(post.daysFromStart, "days")
+              .add(post.daysFromStart, 'days')
               .format(),
             teamMemberID: member.teamMemberID,
             phoneNumber: member.phoneNumber,
@@ -143,7 +143,7 @@ router.post("/assign", async (req, res) => {
             lastName: member.lastName,
             jobDescription: member.jobDescription,
             trainingSeriesID: trainingSeriesID,
-            userID: member.user_ID
+            userID: member.userID
           };
         });
 
@@ -155,31 +155,31 @@ router.post("/assign", async (req, res) => {
       } catch (err) {
         res
           .status(500)
-          .json({ message: "There was an error assembling the information." });
+          .json({ message: 'There was an error assembling the information.' });
       }
     });
 
     res.status(201).json({
-      message: "The team member has been assigned to the training series."
+      message: 'The team member has been assigned to the training series.'
     });
   } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
+    res.status(500).json({ message: 'A network error occurred' });
   }
 });
 
 // Remove team member from a training series
-router.delete("/:id/assign/:ts_id", async (req, res) => {
+router.delete('/:id/assign/:ts_id', async (req, res) => {
   try {
     const { id, ts_id } = req.params;
     const deleted = await TeamMember.removeFromTrainingSeries(id, ts_id);
-    console.log("deleted", deleted);
+    console.log('deleted', deleted);
     if (deleted > 0) {
-      res.status(200).json({ message: "The resource has been deleted." });
+      res.status(200).json({ message: 'The resource has been deleted.' });
     } else {
-      res.status(404).json({ error: "The resource could not be found." });
+      res.status(404).json({ error: 'The resource could not be found.' });
     }
   } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
+    res.status(500).json({ message: 'A network error occurred' });
   }
 });
 
