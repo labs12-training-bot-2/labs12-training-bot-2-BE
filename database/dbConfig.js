8c06d2483f5fa717376347bf1af5cee7de6f21f0
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-24 18:26:25 -0500

    :wrench: Change config variable to conventional NODE_ENV

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
-const environment = process.env.ENVIRONMENT || 'development';
+const environment = process.env.NODE_ENV || 'development';
 
 module.exports = knex(knexConfig[environment]);
