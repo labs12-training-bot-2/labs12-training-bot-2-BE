9065666ab60f55c9af1ce41a6e07e8cfd6ae906c
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-04-29 19:48:40 -0400

    first four endpoint complete

--- routes/trainingSeriesRouter.js
+++ routes/trainingSeriesRouter.js
@@ -7,7 +7,7 @@ const TrainingSeries = require("../database/Helpers/trainingSeries-model");
 //Routes
 
 // GET all training series (not a production endpoint)
-router.get("/", async (req, res) => {
+router.get("/", async (req, res) => {//--- complete per trello spec ---
   try {
     const trainingSeries = await TrainingSeries.find();
     res.status(200).json({ trainingSeries });
@@ -27,12 +27,14 @@ router.get("/posts", async (req, res) => {
 });
 
 // GET training series by ID
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {//--- complete per trello spec ---
   try {
     const { id } = req.params;
     const trainingSeries = await TrainingSeries.findById(id);
     console.log("trainingSeries", trainingSeries);
-    res.status(200).json({ trainingSeries });
+    trainingSeries.length ?
+    res.status(200).json({ trainingSeries }) :
+    res.status(404).json({ message: "sorry, we couldnt find that training series!" })
   } catch (err) {
     res.status(500).json({ message: "A network error occurred: " });
   }
@@ -48,12 +50,12 @@ router.get("/:id/assignments", async (req, res) => {
   }
 });
 // POST a new training series
-router.post("/", async (req, res) => {
+router.post("/", async (req, res) => {//--- complete per trello spec --- 
   try {
     const { title, user_id } = req.body;
 
     if (!title || !user_id) {
-      res.status(400).json({ error: "Client must provide all fields." });
+      res.status(400).json({ error: `Client must provide: ${!title ? "-a title" : ""} ${!user_id ? "-a user ID" : ""}` });
     } else {
       const newTrainingSeries = await TrainingSeries.add(req.body);
       res.status(201).json({ newTrainingSeries });
@@ -65,10 +67,17 @@ router.post("/", async (req, res) => {
 
 // PUT training series information
 router.put("/:id", async (req, res) => {
+    const { title } = req.body;
   try {
-    const { id } = req.params;
-    const updatedTrainingSeries = await TrainingSeries.update(id, req.body);
-    res.status(200).json({ updatedTrainingSeries });
+    if(title || title !== ""){
+        const { id } = req.params;
+        const updatedTrainingSeries = await TrainingSeries.update(id, req.body);
+        updatedTrainingSeries.length ?
+        res.status(200).json({ updatedTrainingSeries }) :
+        res.status(404).json({ message: "sorry, but we couldnt find that training series!" });
+    }else{
+        res.status(400).json({ message: "Please provide a new title for this training series." });
+    }
   } catch (err) {
     res.status(500).json({ message: "A network error occurred" });
   }
