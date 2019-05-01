c77070d5ceabb36145da35c932608df4cd9d2f82
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-01 15:45:02 -0400

    Fixed missed renamed function lost in a merge conflict

--- database/Helpers/notifications-model.js
+++ database/Helpers/notifications-model.js
@@ -11,7 +11,7 @@ module.exports = {
 	markNotificationAsSent,
 	getNotificationByMessageId,
 	getNotificationsToRecalculate,
-	getTrainingSeriesOfNewMessage,
+	getTrainingSeriesAssignmentsOfNewMessage,
 	getUserNotificationCountData,
 	increaseUserNotificationCount,
 	asyncForEach,
