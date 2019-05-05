1b7b782adbe2df88611f98869b0685cd50333d14
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-05 16:37:50 -0500

    :recycle: Update the User model calls in authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,25 +1,24 @@
-const jwt = require("jsonwebtoken");
 const jwtDecode = require("jwt-decode");
-const jwtKey =
-  process.env.JWT_SECRET ||
-  "add a .env file to root of project with the JWT_SECRET variable";
 
-const Users = require("../database/Helpers/user-model.js");
+const Users = require("../models/db/users");
 
-module.exports = {
-  authenticate,
-};
-
-// implementation details
-function authenticate(req, res, next) {
-  //Gets the token
+/**
+ * When applied to a route, it pulls the JWT off of 
+ * the "authorization" header on the request object
+ * and validates that a user has the proper access
+ * permissions
+ * 
+ * @param  {Object} req
+ * @param  {Object} res
+ * @param  {function} next
+ */
+export const authentication = async (req, res, next) => {
   const token = req.get("Authorization");
-  //Decodes the token
-  const decoded = jwtDecode(token);
+  const { email } = jwtDecode(token);
+  const validUser = await Users.find({ 'u.email': email }).first()
 
-  //Checks if token exists, if not send 401 back.
-  if (decoded) {
-    if (Users.findByEmail(decoded.email)) {
+  if (email) {
+    if (validUser) {
       next();
     } else {
       return res.status(401).json({error: "Invalid token"});
@@ -30,24 +29,3 @@ function authenticate(req, res, next) {
     });
   }
 }
-
-// // implementation details
-// function authenticate(req, res, next) {
-//     const token = req.get("Authorization");
-//     const decoded = jwtDecode(token);
-
-//     //   console.log(decoded);
-
-//     if (token) {
-//       jwt.verify(token, jwtKey, (err, decoded) => {
-//         if (err) return res.status(401).json(err);
-//         req.decodedJwt = decoded;
-
-//         next();
-//       });
-//     } else {
-//       return res.status(401).json({
-//         error: "No token provided, token must be set on the Authorization Header",
-//       });
-//     }
-//   }
