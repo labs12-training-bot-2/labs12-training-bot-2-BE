a8f63f18f8565e5653ec06c87ba6aa303a1f3479
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-05 17:55:12 -0500

    :truck: Move fakeData.js to ./helpers/ directory

--- models/seeds/005-trainingSeries_teamMembers.js
+++ models/seeds/005-trainingSeries_teamMembers.js
@@ -1,4 +1,4 @@
-const { createFakeRelationalEntries } = require("../db/fakeData");
+const { createFakeRelationalEntries } = require("../../helpers/fakeData");
 exports.seed = function(knex, Promise) {
   return knex("relational_table").then(function() {
     return knex("relational_table").insert(createFakeRelationalEntries());
--- models/seeds/006-messages.js
+++ models/seeds/006-messages.js
@@ -1,4 +1,4 @@
-const { createFakeMessages } = require("../db/fakeData");
+const { createFakeMessages } = require("../../helpers/fakeData");
 exports.seed = function(knex, Promise) {
   return knex("messages").then(function() {
     return knex("messages").insert(createFakeMessages());
