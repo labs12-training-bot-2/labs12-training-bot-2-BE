dce29de5231235fa544cd2b447e8f5e147b1b6f3
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-05 19:01:29 -0500

    :recycle: Refactor TrainingSeries model

--- models/db/trainingSeries.js
+++ models/db/trainingSeries.js
@@ -3,61 +3,45 @@ const db = require("../dbConfig.js");
 module.exports = {
   add,
   find,
-  findBy,
-  findById,
-  getAllMessages,
-  getTrainingSeriesMessages,
   update,
   remove,
-  getMembersAssigned
 };
 
-function find() {
-  return db("training_series");
+function find(filters) {
+  if (filters) {
+    return db('training_series AS ts')
+      .select(
+        'ts.id',
+        'ts.title',
+        'u.email AS user'
+      )
+      .join('users AS u', { 'ts.user_id': 'u.id' })
+      .where(filters)
+  }
+  return db("training_series AS ts")
+    .select(
+      'ts.id',
+      'ts.title',
+      'u.email AS user'
+    )
+    .join('users AS u', { 'ts.user_id': 'u.id' })
 }
 
-function findBy(filter) {
+function add(series) {
   return db("training_series")
-    .where(filter)
-    .first();
+    .insert(series, ['*'])
+    .then(ts => find({ 'ts.id': ts[0].id }).first());
 }
 
-function findById(id) {
-  return db("training_series").where({ id });
-}
-
-function add(training_series) {
+function update(id, changes) {
   return db("training_series")
-    .insert(training_series)
-    .returning("*");
-}
-
-// not a production function
-function getAllMessages() {
-  return db("messages");
-}
-
-function getTrainingSeriesMessages(id) {
-  return db("messages")
-    .where({ training_series_id: id })
-    .returning("*");
-}
-
-function update(id, series) {
-  return db("training_series")
+    .update(changes, ["*"])
     .where({ id })
-    .update(series, ["*"]);
+    .then(ts => find({ 'ts.id': ts[0].id }).first())
 }
 
 function remove(id) {
   return db("training_series")
     .where({ id })
     .del();
-}
-
-function getMembersAssigned(id) {
-  return db("relational_table as r")
-    .join("team_members as t", "t.id", "r.team_member_id")
-    .select("r.team_member_id", "r.start_date", "t.first_name", "t.last_name")
-    .where("r.training_series_id", id);
 }
