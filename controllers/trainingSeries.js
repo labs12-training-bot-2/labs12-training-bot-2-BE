326dd8b5f3cacc4311e7604358d15826e5452298
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-05 23:32:05 -0500

    :recycle: Refactor training-series controler

--- controllers/trainingSeries.js
+++ controllers/trainingSeries.js
@@ -3,141 +3,106 @@ const router = require("express").Router();
 
 //Models
 const TrainingSeries = require("../models/db/trainingSeries");
-
-//Routes
+const Messages = require('../models/db/messages');
 
 // GET all training series (not a production endpoint)
-router.get("/", async (req, res) => {
-  //--- complete per trello spec ---
-  try {
-    const trainingSeries = await TrainingSeries.find();
-    res.status(200).json({ trainingSeries });
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
-
-// GET all messages (not a production endpoint)
-router.get("/messages", async (req, res) => {
-  try {
-    const messages = await TrainingSeries.getAllMessages();
-    res.status(200).json({ messages });
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
-
-// GET training series by ID
-router.get("/:id", async (req, res) => {
-  //--- complete per trello spec ---
-  try {
-    const { id } = req.params;
-    const trainingSeries = await TrainingSeries.findById(id);
-    console.log("trainingSeries", trainingSeries);
-    trainingSeries.length
-      ? res.status(200).json({ trainingSeries })
-      : res
-          .status(404)
-          .json({ message: "sorry, we couldnt find that training series!" });
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred: " });
-  }
-});
-
-router.get("/:id/assignments", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const assignments = await TrainingSeries.getMembersAssigned(id);
-    res.status(200).json({ assignments });
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
-// POST a new training series
-router.post("/", async (req, res) => {
-  //--- complete per trello spec ---
-  try {
+router.route('/')
+  .get(async (req, res) => {
+      const trainingSeries = await TrainingSeries.find();
+      res.status(200).json({ trainingSeries });
+  })
+  .post(async (req, res) => {
     const { title, user_id } = req.body;
 
-    if (!title || !user_id) {
-      res.status(400).json({
+    if (!title && !user_id) {
+      return res.status(400).json({
         error: `Client must provide: ${!title ? "-a title" : ""} ${
           !user_id ? "-a user ID" : ""
         }`
       });
-    } else {
-      const newTrainingSeries = await TrainingSeries.add(req.body);
-      res.status(201).json({ newTrainingSeries });
     }
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
+    
+    const newTrainingSeries = await TrainingSeries.add(req.body);
+    
+    return res.status(201).json({ newTrainingSeries });
+  })
 
-// PUT training series information
-router.put("/:id", async (req, res) => {
-  //--- complete per trello spec ---
-  const { title } = req.body;
-  try {
-    if (title || title !== "") {
-      const { id } = req.params;
-      const updatedTrainingSeries = await TrainingSeries.update(id, req.body);
-      updatedTrainingSeries.length
-        ? res.status(200).json({ updatedTrainingSeries })
-        : res.status(404).json({
-            message: "sorry, but we couldnt find that training series!"
-          });
-    } else {
-      res.status(400).json({
-        message: "Please provide a new title for this training series."
+router.route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const trainingSeries = await TrainingSeries.find({ 'ts.id': id });
+
+    if (!trainingSeries) {
+      return res.status(404).json({ 
+        message: "sorry, we couldnt find that training series!" 
       });
     }
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
 
-// DELETE a training series
-router.delete("/:id", async (req, res) => {
-  //--- completed per trello spec ---
-  try {
+    return res.status(200).json({ trainingSeries })
+  })
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { title, user_id } = req.body;
+
+    if (!title.length) {
+      return res.status(400).json({
+        message: "New title cannot be an empty string"
+      })
+    }
+
+    const updatedTrainingSeries = await TrainingSeries.update(id, {
+      title, user_id
+    });
+    
+    if (!updatedTrainingSeries) {
+      return res.status(404).json({
+        message: "Sorry! We couldnt find that training series!"
+      });
+    }
+    
+    return res.status(200).json({ updatedTrainingSeries })
+  })
+  .delete(async (req, res) => {
     const { id } = req.params;
     const deleted = await TrainingSeries.remove(id);
-    if (deleted > 0) {
-      res.status(200).json({ message: "The resource has been deleted." });
-    } else {
-      res.status(404).json({ error: "The resource could not be found." });
+
+    if (!deleted) {
+      return res.status(200).json({ 
+        message: "The resource has been deleted." 
+      });
     }
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
-});
+    return res.status(404).json({ error: "The resource could not be found." });
+  });
+
+// TODO: Normalize notifications/relational_table table/db handlers
+// See note in models/db/seriesMembers.js
+
+// router.get("/:id/assignments", async (req, res) => {
+//   const { id } = req.params;
+//   const assignments = await Messages.find({ 'ts.id': id });
+  
+//   return res.status(200).json({ assignments });
+// });
+
 
 // GET all messages in a training series by training series ID
 router.get("/:id/messages", async (req, res) => {
   //--- under construction per trello spec ---
-  try {
     const { id } = req.params;
 
     //get training series by id
-    const trainingSeries = await TrainingSeries.findById(id);
-
-    //get all messages of training series
-    const messages = await TrainingSeries.getTrainingSeriesMessages(id);
+    const trainingSeries = await TrainingSeries.find({ 'ts.id': id });
 
     if (!trainingSeries.length) {
-      res
-        .status(404)
-        .json({ message: "Sorry, we couldnt find that training series!" });
+      return res.status(404).json({ 
+        message: "Sorry, we couldnt find that training series!" 
+      });
     }
-    // else if(!messages.length){ //not sure if this is useful or not, will probably handel this on the front end, but i wrote so yea, here ya go :)
-    //     res.status(200).json({ trainingSeries, message: "This training series dosnt currently include any messages."});
-    // }
-    else {
-      res.status(200).json({ trainingSeries, messages });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
+    
+    //get all messages of training series
+    const messages = await Messages.find({ 'ts.id': id });
+
+    return res.status(200).json({ trainingSeries, messages });
 });
+
 module.exports = router;
