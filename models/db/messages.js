364cd2de945a46f4ad7a7edcb99f656804fcea16
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-05 19:03:11 -0500

    :recycle: Refactor messages model

--- models/db/messages.js
+++ models/db/messages.js
@@ -1,39 +1,39 @@
-const db = require("../dbConfig.js");
+const db = require("../index.js");
 
 module.exports = {
   add,
   find,
-  findBy,
-  findById,
   update,
   remove
 };
 
-function find() {
-  return db("messages");
-}
-
-function findBy(filter) {
-  return db("messages")
-    .where(filter)
-    .first();
-}
-
-function findById(id) {
-  return db("messages").where({ id });
-}
-
 function add(message) {
   return db("messages")
-    .insert(message)
-    .returning("*");
+    .insert(message, ['*'])
+    .then(m => find({ 'm.id': m[0].id }).first())
+}
+
+function find(filters) {
+  return db('messages AS m')
+    .select(
+      'm.id',
+      'm.message_name',
+      'm.message_details',
+      'm.link',
+      'm.days_from_start',
+      'ts.title AS series'
+    )
+    .leftJoin('training_series AS ts', { 'ts.id': 'm.training_series_id' })
+    .leftJoin('users AS u', { 'u.id': 'ts.user_id' })
+    .where(filters)
+    .orderBy('series')
 }
 
 function update(id, message) {
   return db("messages")
+    .update(message, ['*'])
     .where({ id })
-    .update(message)
-    .returning("*");
+    .then(m => find({ 'm.id': m[0].id }).first())
 }
 
 function remove(id) {
