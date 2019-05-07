46dc15203a9cb5b34ae41ec627e05908d14bb046
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-05-07 13:51:27 -0500

    :sparkles: Joi implemented and tested in messages
    Joi validation middleware written and implemented in messages, will need to be implemented separately in each controller file. Will be able to delete 400 case errors in each controller file

--- middleware/dataValidation.js
+++ middleware/dataValidation.js
@@ -4,7 +4,9 @@ module.exports = schema => {
   return (req, res, next) => {
     const { error } = Joi.validate(req.body, schema);
     if (error) {
-      return res.status(400).json({ error: error.details });
+      return res.status(400).json({
+        error: `Error during ${req.method} request: ${error.details[0].message}`
+      });
     }
     next();
   };
