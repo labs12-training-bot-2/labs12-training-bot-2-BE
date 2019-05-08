7d47a22c5b0d23dbdbc3622a441b05efa7bf9428
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-08 17:36:25 -0500

    :sparkles: Delete notifications that failed to send

--- jobs/notifications/lib/wipeFailed.js
+++ jobs/notifications/lib/wipeFailed.js
@@ -1,21 +1,13 @@
 const Notifications = require('../../../models/db/notifications')
 
 module.exports = async time => {
-  console.log('Wiping failed notifications', time);
+  Promise.resolve(console.log('Wiping failed notifications', time));
   
-  const records = await Notifications.find({
-    'n.num_sent': 7, 'n.is_sent': false
-  })
+  // const deletion = await Notifications.remove({
+  //   'n.num_attempts': 7, 'n.is_sent': false
+  // }).catch(e => console.error(e))
 
-  const failed = records.map(async r => {
-    return await Notifications.remove({ 'n.id': r.id })
-  })
-
-  Promise.all(failed)
-    .then(() => {
-      console.log('All failed notifications have been removed')
-    })
-    .catch(err => {
-      console.error('Error wiping failed notifications:', err)
-    })
-}
+  // return deletion 
+  //   ? `${deletion} records deleted` 
+  //   : `There was a problem deleting those records: ${deletion}`
+  }
