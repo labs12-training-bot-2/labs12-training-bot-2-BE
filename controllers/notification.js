31b982c2345b2d5a069c6764e8c19f8f89e46cd4
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-05-08 11:12:58 -0500

    :art: Updated controller to grab responses
    Grab all responses linked to associated id in /:id/responses endpoint here instead of /api/responses, since there's easy access to notification id here already. Altered delete to only be possible in /:id/delete as well to reduce risk of accidental deletion elsewhere

--- controllers/notification.js
+++ controllers/notification.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 const Notifications = require("../models/db/notifications");
 const Messages = require("../models/db/messages");
 const TeamMembers = require("../models/db/teamMembers");
+const Responses = require("../models/db/responses");
 
 // Validation
 const { notificationSchema } = require("../models/schemas");
@@ -54,8 +55,24 @@ router.route("/:id").get(async (req, res) => {
     : res.status(404).json({ message: "That notification does not exist." });
 });
 
+router.route("/:id/responses").get(async (req, res) => {
+  const { id } = req.params;
+  const { email } = res.locals.user;
+
+  const notification = await Notifications.find({
+    "n.id": id,
+    "u.email": email
+  });
+  if (notification && notification.length) {
+    const responses = await Responses.find({ "r.notification_id": id });
+    res.status(200).json({ responses });
+  } else {
+    res.status(404).json({ message: "That notification does not exist." });
+  }
+});
+
 //client forced to go to this route specifically to make accidental deletions a smaller risk
-router.route("/delete/:id").delete(async (req, res) => {
+router.route(":id/delete").delete(async (req, res) => {
   const { id } = req.params;
   const { email } = res.locals.user;
 
