1f649034eac24c174d587f816bc06981eefd19a8
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-09 16:40:48 -0500

    :sparkles: Create async version of the Map function

--- jobs/notifications/lib/common.js
+++ jobs/notifications/lib/common.js
@@ -1,19 +1,34 @@
-const db = require('../../../models');
+const db = require("../../../models");
 
 module.exports = {
-  batchUpdate
+  batchUpdate,
+  asyncMap
 };
 
 function batchUpdate(table, arr) {
+  console.log("initial values", table, arr);
   return db.transaction(trx => {
-    const queries = arr.map(item =>
+    const queries = arr.map(i =>
       db(table)
-        .where("id", item.id)
-        .update(item)
+        .where("id", i.id)
+        .update(i)
         .transacting(trx)
     );
+
     return Promise.all(queries)
       .then(trx.commit)
       .catch(trx.rollback);
   });
+}
+
+async function asyncMap(arr, cb) {
+  const pArray = arr.map(async i => {
+    const promise = await cb(i);
+
+    return promise;
+  });
+
+  const data = await Promise.all(pArray);
+
+  return data;
 }
