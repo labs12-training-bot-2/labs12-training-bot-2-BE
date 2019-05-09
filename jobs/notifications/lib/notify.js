2992f39d0a91d5044aef2cd1da7fc245fade12c5
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-09 17:20:51 -0500

    :memo: Comment the notify() function

--- jobs/notifications/lib/notify.js
+++ jobs/notifications/lib/notify.js
@@ -13,15 +13,17 @@ module.exports = async time => {
       time
     );
 
+    // Organize notifications by service
     const slackNotifs = notifs.filter(n => n.name === "slack");
     const smsNotifs = notifs.filter(n => n.name === "twilio");
     const emailNotifs = notifs.filter(n => n.name === "sendgrid");
 
+    // Send notifications by service
     const sendingSlack = await asyncMap(slackNotifs, sendSlack);
     const sendingSms = await asyncMap(smsNotifs, sendSms);
     const sendingEmail = await asyncMap(emailNotifs, sendEmail);
 
-    // Update num_attempts and is_sent for all notifications
+    // Combine notifications from all services and batch update the database
     const updates = [...sendingSms, ...sendingEmail, ...sendingSlack]
     batchUpdate("notifications", updates);
 
