4ab13833033d2ad5678543093525cf56a9e11b3b
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-08 19:27:34 -0500

    :construction: Add wipeFailed function to remove failed records

--- jobs/notifications/lib/wipeFailed.js
+++ jobs/notifications/lib/wipeFailed.js
@@ -1,13 +1,11 @@
 const Notifications = require('../../../models/db/notifications')
 
 module.exports = async time => {
-  Promise.resolve(console.log('Wiping failed notifications', time));
-  
-  // const deletion = await Notifications.remove({
-  //   'n.num_attempts': 7, 'n.is_sent': false
-  // }).catch(e => console.error(e))
+  const deletion = await Notifications.remove({
+    'n.num_attempts': 7, 'n.is_sent': false
+  }).catch(e => console.error(e))
 
-  // return deletion 
-  //   ? `${deletion} records deleted` 
-  //   : `There was a problem deleting those records: ${deletion}`
+  return deletion 
+    ? `${deletion} records deleted` 
+    : `There was a problem deleting those records: ${deletion}`
   }
