228d0990f860be88063c0469fca0697b3df79d46
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-10 14:33:41 -0500

    :memo: Document middleware functions

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,3 +1,16 @@
+/**
+ * Middleware to catch all errors occurring on all routes.
+ * 
+ * Ensures that we don't need to use Try/Catch blocks inside
+ * of our routes.
+ * 
+ * @module errorHandling
+ * @function
+ * @param  {Object} err - A JavaScript error object
+ * @param  {Object} req - Express request object
+ * @param  {Object} res - Express response object
+ * @param  {function} next - Express next middleware function
+ */
 module.exports = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const env = process.env.NODE_ENV || "development";
