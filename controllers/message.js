af7f0e3e1d1ce44939fa94fda741b33f972a8a90
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-05-14 15:12:03 -0500

    :bug: Make initial find() query in delete to avoid raw
    Make message find() query in delete /:id route before deletion to detect 404 case or not--otherwise would require lengthy knex.raw query for deletion since knex does not support deletion when joins are used

--- controllers/message.js
+++ controllers/message.js
@@ -57,11 +57,17 @@ router
   .delete(async (req, res) => {
     const { id } = req.params;
     const { email } = res.locals.user;
-    const deleted = await Messages.remove({ "m.id": id, "u.email": email });
 
-    deleted
-      ? res.status(200).json({ message: "The message has been deleted." })
-      : res.status(404).json({ error: "That message does not exist." });
+    const message = await Messages.find({
+      "m.id": id,
+      "u.email": email
+    }).first();
+    if (!message) {
+      return res.status(404).json({ message: "That message does not exist" });
+    }
+
+    await Messages.remove({ "m.id": id });
+    res.status(200).json({ message: "The message has been deleted." });
   });
 
 module.exports = router;
