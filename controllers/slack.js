15a21aa1767e144effe2994226db71d05f592d8f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-17 15:29:56 -0400

    Added console.log

--- controllers/slack.js
+++ controllers/slack.js
@@ -16,9 +16,9 @@ router.post("/oauth/", async ({ body: { code } }, res) => {
     process.env.SLACK_SECRET
   }&code=${code}&redirect_uri=${process.env.APP_BASE_URL}/slack-callback`;
   const url = `https://slack.com/api/oauth.access?${query}`;
-  console.log("URL", url);
 
   const auth_res = await axios.get(url);
+  console.log("auth_res", auth_res);
   const { id } = res.locals.user;
   const token = {
     user_id: id,
