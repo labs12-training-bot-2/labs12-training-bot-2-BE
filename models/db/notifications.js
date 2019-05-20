4cbf0dd395d9be78cd00da6b350c8c4198b7b8aa
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-20 16:14:55 -0500

    :memo: Document the Notifications model

--- models/db/notifications.js
+++ models/db/notifications.js
@@ -3,10 +3,31 @@ const db = require("../index");
 module.exports = {
   add,
   find,
-  remove,
-  update
+  update,
+  remove
 };
 
+/**
+ * Adds a new Notification to the database, and then returns the newly created
+ * Notification
+ *
+ * @function
+ * @param  {Object} message - A Notification object
+ * @returns {Promise} Promise that resolves to the new Notification object
+ */
+function add(notification) {
+  return db("notifications")
+    .insert(notification, ["*"])
+    .then(n => find({ "n.id": n[0].id }).first());
+}
+
+/**
+ * Finds a particular Notification or set of Notifications based on the key/
+ * value pairs contained within the filter object parameter.
+ *
+ * @param  {Object} filters - A filter object to be passed to the "where" clause
+ * @returns {Promise} Promise that resolves to an array of found Notification objects
+ */
 function find(filters) {
   return db("notifications AS n")
     .select(
@@ -38,18 +59,27 @@ function find(filters) {
     .orderBy("n.send_date");
 }
 
-function add(notification) {
-  return db("notifications")
-    .insert(notification, ["*"])
-    .then(n => find({ "n.id": n[0].id }).first());
-}
-
+/**
+ * Updates a Notification or set of Notifications based on the key/value pairs
+ * contained within the filter object parameter.
+ *
+ * @param  {Object} filter - A filter object to be passed to the "where" clause
+ * @param  {Object} changes - An object containing the changes you'd like to make to the Message(s) selected by the filter object
+ * @returns {Promise}
+ */
 function update(filter, changes) {
   return db("notifications AS n")
     .update(changes, ["*"])
     .where(filter);
 }
 
+/**
+ * Deletes a single Notification or set of Notifications based on the key/value
+ * pairs contained within the filter object parameter.
+ *
+ * @param  {Object} filter - A filter object to be passed to the "where" clause
+ * @returns {Promise} A Promise that resolves to the number of Notifications deleted
+ */
 function remove(filters) {
   return db("notifications AS n")
     .leftJoin("team_members AS tm", { "tm.id": "n.team_member_id" })
