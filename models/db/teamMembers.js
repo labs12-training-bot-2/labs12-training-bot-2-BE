6e57503d36a6256fd73edbdfb48a87d0233693cb
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-21 14:57:11 -0500

    :memo: Document the Team Members Model

--- models/db/teamMembers.js
+++ models/db/teamMembers.js
@@ -7,6 +7,15 @@ module.exports = {
   remove
 };
 
+/**
+ * Find a Team Member or set of Team Members in the database
+ *
+ * @function
+ * @param {Object} filters - A filters object to pass to the SQL WHERE clause
+ * @see https://knexjs.org/#Builder-where
+ *
+ * @returns {Promise} - A Promise that resolves to an array of Team Member objects
+ */
 function find(filters) {
   return db("team_members AS tm")
     .select(
@@ -29,24 +38,52 @@ function find(filters) {
     .where(filters);
 }
 
-function add(member) {
+/**
+ * Add a Team Member to the Database
+ *
+ * @function
+ * @param {Object} teamMember - A Team Member object
+ * @see https://knexjs.org/#Builder-insert
+ *
+ * @returns {Promise} - A Promise that resolves to the newly created Team Member
+ */
+function add(teamMember) {
   return db("team_members")
-    .insert(member, ["*"])
+    .insert(teamMember, ["*"])
     .then(tm => find({ "tm.id": tm[0].id }).first());
 }
 
-async function update(id, member) {
+/**
+ * Update a Team Member to the Database
+ *
+ * @function
+ *
+ * @param {Object} filters - A filters object to pass to the SQL WHERE clause
+ * @see https://knexjs.org/#Builder-where
+ *
+ * @param {Object} changes - An object representing the keys to update and their values
+ * @see https://knexjs.org/#Builder-update
+ *
+ * @returns {Promise} - A Promise that resolves to the updated Team Member(s)
+ */
+function update(filters, changes) {
   return db("team_members")
-    .where({
-      id
-    })
-    .update(member);
+    .update(changes, ["*"])
+    .where(filters)
+    .then(tm => find({ "tm.id": tm[0].id }).first());
 }
 
-function remove(id) {
-  return db("team_members")
-    .where({
-      id
-    })
+/**
+ * Deletes a specific Team Member or set of Team Members from the database
+ *
+ * @function
+ * @param {Object} filters - A filters object to pass to the SQL WHERE clause
+ * @see https://knexjs.org/#Builder-where
+ * 
+ * @returns {Promise} A promise that resolves to the number of Team Member(s) deleted
+ */
+function remove(filters) {
+  return db("team_members AS tm")
+    .where(filters)
     .del();
 }
