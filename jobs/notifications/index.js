35a4178be788d4e600f5deb9f81f1fb09205c070
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-21 17:41:40 -0500

    :sparkles: Convert the Notifications cronjob to an IIFE

--- jobs/notifications/index.js
+++ jobs/notifications/index.js
@@ -2,42 +2,63 @@ const CronJob = require("cron").CronJob;
 const notify = require("./lib/notify");
 const wipeFailed = require("./lib/wipeFailed");
 
-// node-cron start function for notification system
-const notificationSystem = function() {
-  return {
-    start: () => {
+(function notificationSystem() {
+  /**
+   * A CronJob object.
+   *
+   * @constructor
+   *
+   * @param time {String} - A crontab string to specify when the job should run.
+   * @see https://github.com/kelektiv/node-cron#cron-ranges
+   *
+   * @param onTick {function} - The function to fire at the time specified. If an onComplete callback was provided, onTick will receive it as an argument. onTick may call onComplete when it has finished its work.
+   *
+   * @param onComplete {function} - The function that will fire when the onTick function ends
+   *
+   * @param start {boolean} - Specifies whether to start the job before exiting the constructor.
+   *
+   * @param timezone {string} - The timezone for the execution. See linked documentation below for timezone options, though I highly recommend leaving this in UTC
+   * @see http://momentjs.com/timezone/
+   */
       new CronJob(
         "0 */10 * * * *",
         async function(onComplete) {
+      /**
+       * An anonomous function run by CronJob at the interval specified in the CronTab. Runs the notify() function and then calls the onComplete function passed to it by CronJob
+       *
+       * @param [onComplete] {function} - An optional param that gets passed in by the constructor if it exists
+       */
           try {
+        // Get the current time for logging and also to pass to notify()
             const currentTime = new Date();
+
+        // Log that the job has started on the server
             console.log("Run Notifications onTick:", currentTime);
+
+        // Await the completion of the notify() function, exit to catch block if notify() rejects
             await notify(currentTime);
+
+        // Await the completion of the onComplete function, exit to catch block if onComplete() rejects
             await onComplete(currentTime);
-            
-            // Log the completion of the Notification event
-            console.log("Notifications sent:", new Date());
           } catch (error) {
-            console.log("Notification start async error", error);
+        // If the onTick function fails at any point, log the error to stderr
+        console.error("Notifications worker encountered an error", error);
           }
         },
         async function(time) {
-          try {
+      /**
+       * An anonomous function that will be passed to the onTick function and called at the end of that function. Runs the wipeFailed() function.
+       *
+       * @param time {DateTime} - A JavaScript Datetime field
+       */
+      
+      // Await the completion of the wipeFailed() function, exit to catch block in the onTick function if wipeFailed() rejects
             await wipeFailed(time);
-          } catch (error) {
-            console.error(
-              "There was an error removing outdated notifications",
-              error
-            );
-          }
         },
         true,
         "UTC"
       );
-      console.log("Notification System Instantiation");
-      console.log(new Date());
-    }
-  };
-};
 
-module.exports = notificationSystem();
+  // Log that the Notification System has been started
+  console.log("Notification System Instantiation", new Date());
+})();
